2bdb7a9d222ec21adcee01114121aeebc3414c1a
Al En <gitaen@alenri.net>
2013-12-21 19:29:29 +0100

    adding simple python plotter

---
+++ plotter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import Gnuplot
+import serial
+import sys
+import time
+
+ser = serial.Serial(sys.argv[1], 9600)
+
+temp = []
+power = []
+
+g = Gnuplot.Gnuplot()
+g.title("Temperature")
+g('set style data lines')
+
+h = Gnuplot.Gnuplot()
+h.title("Heater")
+h('set style data lines')
+
+start = int(time.time())
+
+while 1:
+    reading = ser.readline().split()
+    print reading
+    
+    now = int(time.time())-start
+    temp.append([now, reading[0]])
+    power.append([now, reading[5]])
+
+    if len(temp) > 60:
+        temp = temp[-60:]
+        power = power[-60:]
+
+    g.plot(temp)
+    h.plot(power)
